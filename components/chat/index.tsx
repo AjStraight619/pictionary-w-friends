e456541a0ce450f53f313a27e3a5c09073f0090e
Alex Straight <alex@Alexs-Laptop.local>
2024-05-18 22:02:57 -0700

    added to chat component

--- components/chat/index.tsx
+++ components/chat/index.tsx
@@ -1,11 +1,26 @@
-import { Card, CardHeader, CardTitle } from "../ui/card";
+"use client";
+import { useStorage } from "@/liveblocks.config";
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "../ui/card";
+import { Input } from "../ui/input";
 
 export default function Chat() {
+  const messages = useStorage((root) => root.messages);
+
   return (
     <Card>
       <CardHeader>
         <CardTitle>Chat</CardTitle>
       </CardHeader>
+      <CardContent></CardContent>
+      <CardFooter>
+        <Input />
+      </CardFooter>
     </Card>
   );
 }
